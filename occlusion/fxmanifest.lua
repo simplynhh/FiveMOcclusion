c80948a516ef8b9ac83ca6b49683cf961c52604e
Ng Hong Han <63585748+simplynhh@users.noreply.github.com>
2021-09-01 03:19:50 +0800

    Add files via upload

---
+++ occlusion/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file 'AUDIO_GAMEDATA' 'audio/restauracja_game.dat' -- dat151
+data_file 'AUDIO_DYNAMIXDATA' 'audio/restauracja_mix.dat' -- dat15
+
+files {
+  'audio/restauracja_game.dat151.rel',
+  'audio/restauracja_mix.dat15.rel',
+
+}
